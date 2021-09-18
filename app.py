6e727962ebc4e490ef9a8e95198d1eb4f110c466
teja <tejanidhi18@gmail.com>
2021-09-18 17:51:12 +0530

    added logs

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, send_from_directory
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
 from scripts.services import user_data_service
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(filename='demo.log', level=logging.DEBUG)
 
 app.register_blueprint(user_data_service.user_data_status)
 
